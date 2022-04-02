2d83dc5ff85cdf935b3a5adbdb5eaf15a23e55b7
Kirill Rozov <kirill@androidbroadcast.dev>
2022-04-02 13:47:11 +0300

    Refactor annotations

--- analytics-proxy/src/main/kotlin/dev/androidbroadcast/analyticsproxy/Annotations.kt
+++ analytics-proxy/src/main/kotlin/dev/androidbroadcast/analyticsproxy/Annotations.kt
@@ -1,5 +1,9 @@
 package dev.androidbroadcast.analyticsproxy
 
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class EventName(val value: String)
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Param(val value: String)
--- analytics-proxy/src/main/kotlin/dev/androidbroadcast/analyticsproxy/EventName.kt
+++
@@ -1,5 +0,0 @@
-package dev.androidbroadcast.analyticsproxy
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class EventName(val value: String)
