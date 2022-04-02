1139c8f83f9c9121fc4fc9025caad6f2e5026a4e
Kirill Rozov <kirill@androidbroadcast.dev>
2022-04-02 19:00:52 +0300

    Add cached property to builder

--- analytics-proxy/src/main/kotlin/dev/androidbroadcast/analyticsproxy/AnalyticsProxy.kt
+++ analytics-proxy/src/main/kotlin/dev/androidbroadcast/analyticsproxy/AnalyticsProxy.kt
@@ -3,28 +3,37 @@ package dev.androidbroadcast.analyticsproxy
 import java.lang.reflect.Proxy
 import kotlin.properties.Delegates
 
-class AnalyticsProxy(private val analyticsTracker: AnalyticsTracker) {
+class AnalyticsProxy private constructor(
+    private val analyticsTracker: AnalyticsTracker,
+    private val cached: Boolean
+) {
 
     @Suppress("UNCHECKED_CAST")
     fun <T : Any> create(clazz: Class<T>): T {
         return Proxy.newProxyInstance(
             clazz.classLoader,
             arrayOf(clazz),
-            AnalyticsProxyInvocationHandler(analyticsTracker)
+            AnalyticsProxyInvocationHandler(analyticsTracker, cached)
         ) as T
     }
 
-    class Builder() {
+    class Builder {
 
         private var analyticsTracker: AnalyticsTracker by Delegates.notNull()
+        private var cached = true
 
         fun analyticsTracker(analyticsTracker: AnalyticsTracker): Builder {
             this.analyticsTracker = analyticsTracker
             return this
         }
 
+        fun cached(cached: Boolean): Builder {
+            this.cached = cached
+            return this
+        }
+
         fun build(): AnalyticsProxy {
-            return AnalyticsProxy(analyticsTracker)
+            return AnalyticsProxy(analyticsTracker, cached)
         }
     }
 }
